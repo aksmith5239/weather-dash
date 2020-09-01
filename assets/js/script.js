09b39a9f0ef70cf0ff8cd572f0be64eef621caaa
aksmith5239 <aklobby@gmail.com>
2020-09-01 17:22:11 -0600

    search city form completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,37 @@
 var searchFormEl = document.querySelector("#search-form");
-var key = "8fe2f99f82675d0a0ea6fc0f216bf16d";
-var city = "";
-var state = "";
-var country = "";
-var searchName = ""; // this is a variable that will hold a concatenated city, state, country
-// note cities with two names must be joined with a plus sign
+var searchNameEl = document.querySelector("#search"); // this is a variable that will hold a concatenated city, state, country
 
-// this function is not yet ready to work: 
 
-// var getWeather = function(cityName) {
-    
+var getWeather = function(searchName) {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchName + "&APPID=8fe2f99f82675d0a0ea6fc0f216bf16d";
+    fetch (apiUrl)
+            .then(function(response){
+                
+                if(response.ok) {
+                    return response.json()
+                    .then(function(response) {
+                        console.log(response);
+                        //once the display function is complete we will replace the second .then funciton with
+                        //.then(function(data){
+
+                        //}) 
+                    })
+                } else {
+                    alert("Error: " + response.statusText);
+                }
+                
+            })
+            .catch(function(error) {
+                alert("Unable to connect to Open Weather");
+            })
+            
+
+
     // //temporary apiUrl
     // var apiUrl= "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=8fe2f99f82675d0a0ea6fc0f216bf16d";
     
-    // //use this apiUrl when we get the search working
-    // // var apiUrl =  "http://api.openweathermap.org/data/2.5/weather?q=Las+Cruces,NM,us&appid=8fe2f99f82675d0a0ea6fc0f216bf16d;
+    // // //use this apiUrl when we get the search working
+    // // var apiUrl =  "http://api.openweathermap.org/data/2.5/weather?q=" + searchName + " &appid=8fe2f99f82675d0a0ea6fc0f216bf16d";
 
     // fetch(apiUrl).then(function(response) {
     //     conosole.log(response);
@@ -23,30 +40,38 @@ var searchName = ""; // this is a variable that will hold a concatenated city, s
                 
     //         });
     //     } else {
-    //         alert("Error: " + response.statusText);
+    //         console.log("Error: " + response.statusText);
     //     }
     // })
     // .catch(function(error){
-    //     alert("Unable to connect to Open Weather");
-    // })
-// }
+    //     console.log("Unable to connect to Open Weather");
+    // });
+}
 
-// getWeather();
-
-// var formSubmitHandler = function(event) {
- // here we will get the information from the search box, combine the city state and country into the searchCity variable.
- 
-//}
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var searchName = searchNameEl.value.trim();
+    console.log(searchName);
+    if (searchName) {
+        getWeather(searchName);
+        searchNameEl.value = "";
+    } else {
+        alert("Please enter a city name.");
+    }
+} // end of formSubmitHandler
 
 
 // this is currrently fetching info from openweather
-fetch (
-    "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=8fe2f99f82675d0a0ea6fc0f216bf16d"
-)
-.then(function(response){
-    console.log(response);
-    return response.json();
-})
-.then(function(response) {
-    console.log(response);
-})
+// fetch (
+//     "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=8fe2f99f82675d0a0ea6fc0f216bf16d"
+// )
+// .then(function(response){
+//     console.log(response);
+//     return response.json();
+// })
+// .then(function(response) {
+//     console.log(response);
+// })
+
+// event listener
+searchFormEl.addEventListener("submit", formSubmitHandler);
