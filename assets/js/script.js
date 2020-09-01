bc395903dcdcc89015d89e5f0579a78aaf722a80
aksmith5239 <aklobby@gmail.com>
2020-09-01 13:26:50 -0600

    starting work on search functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,36 @@
+var searchFormEl = document.querySelector("#search-form");
+var key = "8fe2f99f82675d0a0ea6fc0f216bf16d";
+var cityName = "";
+
+// this function is not yet ready to work: 
+
+// var getWeather = function(cityName) {
+    
+    // //temporary apiUrl
+    // var apiUrl= "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=8fe2f99f82675d0a0ea6fc0f216bf16d";
+    
+    // //use this apiUrl when we get the search working
+    // // var apiUrl =  "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key;
+
+    // fetch(apiUrl).then(function(response) {
+    //     conosole.log(response);
+    //     if(response.ok) {
+    //         response.json().then(function(data){
+                
+    //         });
+    //     } else {
+    //         alert("Error: " + response.statusText);
+    //     }
+    // })
+    // .catch(function(error){
+    //     alert("Unable to connect to Open Weather");
+    // })
+// }
+
+// getWeather();
+
+
+// this is currrently fetching info from openweather
 fetch (
     "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=8fe2f99f82675d0a0ea6fc0f216bf16d"
 )
