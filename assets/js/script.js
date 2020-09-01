b1c2c6dc1f8885d477f1d635253adc197539c892
aksmith5239 <aklobby@gmail.com>
2020-09-01 14:49:00 -0600

    saved work on search city functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 var searchFormEl = document.querySelector("#search-form");
 var key = "8fe2f99f82675d0a0ea6fc0f216bf16d";
-var cityName = "";
+var city = "";
+var state = "";
+var country = "";
+var searchName = ""; // this is a variable that will hold a concatenated city, state, country
+// note cities with two names must be joined with a plus sign
 
 // this function is not yet ready to work: 
 
@@ -10,7 +14,7 @@ var cityName = "";
     // var apiUrl= "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=8fe2f99f82675d0a0ea6fc0f216bf16d";
     
     // //use this apiUrl when we get the search working
-    // // var apiUrl =  "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key;
+    // // var apiUrl =  "http://api.openweathermap.org/data/2.5/weather?q=Las+Cruces,NM,us&appid=8fe2f99f82675d0a0ea6fc0f216bf16d;
 
     // fetch(apiUrl).then(function(response) {
     //     conosole.log(response);
@@ -28,6 +32,11 @@ var cityName = "";
 // }
 
 // getWeather();
+
+// var formSubmitHandler = function(event) {
+ // here we will get the information from the search box, combine the city state and country into the searchCity variable.
+ 
+//}
 
 
 // this is currrently fetching info from openweather
