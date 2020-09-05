ec14b26c4659228dbca1e6015cd498be2ad05c75
aksmith5239 <aklobby@gmail.com>
2020-09-05 11:19:20 -0600

    current city work updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 m = moment();
-var currentDay = m.format('dddd, MMMM Do');
+var currentDay = m.format('M/d/YYYY');
 console.log(currentDay);
 var searchFormEl = document.querySelector("#search-form");
 var searchCityEl = document.querySelector("#search-city"); // this is a variable that will hold a concatenated city, state, country
 var cityContainerEl = document.querySelector("#city-container");
+var titleEl = document.querySelector("#city-title");
 
 var getWeather = function(searchCity) {
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&APPID=8fe2f99f82675d0a0ea6fc0f216bf16d";
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&units=imperial&APPID=8fe2f99f82675d0a0ea6fc0f216bf16d";
     fetch (apiUrl)
             .then(function(response){
                 if(response.ok) {
@@ -62,13 +63,46 @@ var displayCurrentCity = function(searchCity) {
     var city = searchCity.name;
     var windSpeed = searchCity.wind.speed;
     var temperature = searchCity.main.temp;
-
+    
+    var weatherIcon = searchCity.weather[0].icon;
+    var weatherIconUrl = "http://openweathermap.org/img/wn/" + weatherIcon + ".png";
+    var humidity = searchCity.main.humidity;
     // var windSpeed = SearchCity.wind.speed;
     console.log(city);
     console.log(windSpeed);
     console.log(temperature);
-    // console.log(windSpeed);
-    // var to call searchCity result as city
+    console.log(weatherIcon);
+    console.log(humidity);
+
+    //clear old content
+    searchCityEl.textContent = "";
+
+
+    //create our elements:
+    var cityTitleEl = document.createElement("span");
+    cityTitleEl.innerHTML = (city + " (" + currentDay + ")" + "<img src='" + weatherIconUrl + "'>");
+    // console.log(cityTitleEl);
+    // console.log(weatherIconUrl);
+    titleEl.appendChild(cityTitleEl);
+
+    var cityTemp = document.createElement("li");
+    cityTemp.classList = "list-group-item flex-row justify-space-between align-left border-0";
+    cityTemp.textContent = ("Temperature: " + temperature + " °F");
+    console.log(cityTemp);
+    
+    var cityHumidity = document.createElement("li");
+    cityHumidity.classList = "list-group-item flex-row justify-space-between align-left border-0";
+    cityHumidity.textContent = ("Humidity: " + humidity + "%");
+    console.log(cityHumidity);
+    
+    var cityWindSpeed = document.createElement("li");
+    cityWindSpeed.classList = "list-group-item flex-row justify-space-between align-left border-0";
+    cityWindSpeed.textContent = ("Wind Speed: " + windSpeed + " MPH");
+    console.log(cityWindSpeed);
+    
+    cityContainerEl.appendChild(cityTemp);
+    cityContainerEl.appendChild(cityHumidity);
+    cityContainerEl.appendChild(cityWindSpeed);
     // need current date
     // weather icon weather.icon
     // Name of city  name
